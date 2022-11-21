c4df1042c3923ee9aa747efcf666081461b6be19
ARY2260 <arb.aryan@gmail.com>
2022-11-21 23:36:03 +0530

    initial setup of backend

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+// routes
+
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Plag Check activated!'));
+
+// use Routes
+
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
